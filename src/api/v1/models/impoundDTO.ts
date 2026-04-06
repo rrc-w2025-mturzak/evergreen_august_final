4db32dd7a0e42a9f731bf53c163d31da09f79079
August <mturzak@rrc.ca>
2026-04-06 18:11:07 -0500

    added impoundDTO

---
+++ src/api/v1/models/impoundDTO.ts
@@ -0,0 +1,9 @@
+export interface ImpoundDTO {
+    id: string;
+    plateNumber: string;
+    vehicleType: string;
+    color: string;
+    daysInLot: number;
+    realeaseFee: number;
+    createdAt: Date;
+}
