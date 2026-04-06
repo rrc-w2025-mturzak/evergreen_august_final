32a93feb8b923451dd24b341bcf6960d985cd45f
August <mturzak@rrc.ca>
2026-04-06 18:15:07 -0500

    added getImpoundById to firebaseRepository

--- src/api/v1/repositories/firestoreRepository.ts
+++ src/api/v1/repositories/firestoreRepository.ts
@@ -21,3 +21,24 @@ export const addImpound = async (item:ImpoundCreateRequest): Promise<string> => 
     return docRef.id;
 };
 
+export const getImpoundById = async (id: string): Promise<ImpoundDTO | undefined> => {
+    const docRef: DocumentReference = db.collection("Impounds").doc(id);
+
+    const doc = await docRef.get();
+
+    if (doc.exists) {
+        let data = doc.data();
+
+        return {
+            id: doc.id,
+            plateNumber: data!.plateNumber,
+            vehicleType: data!.vehicleType,
+            color: data!.color,
+            daysInLot: data!.daysInLot,
+            realeaseFee: data!.releaseFee,
+            createdAt: data!. createdAt
+        }
+    } else {
+        console.log("No such Impound!");
+    }
+};
